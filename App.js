bfb9c49204044a3149abc85669b74cda4767ae99
Rohit Sharma <rohit.sharma@browserstack.com>
2025-12-15 09:45:26 +0530

    Chapter1_inception

---
+++ App.js
@@ -0,0 +1,22 @@
+const parent = React.createElement('div', {id: 'parent'},
+    [
+        React.createElement('div', {id: 'child'},
+        [React.createElement('h1', {id: 'heading1'}, 'I am h1 tag from React'),
+         React.createElement('h2', {id: 'heading2'}, 'I am h2 tag from React')]
+    ),
+        React.createElement('div', {id: 'child2'},
+        [
+            React.createElement('h1', {id: 'heading1'}, 'I am h1 tag from React'),
+            React.createElement('h2', {id: 'heading2'}, 'I am h2 tag from React')
+        ]
+    )
+    ]
+);
+
+
+
+//const heading = React.createElement('h1', {id: 'heading'}, 'Hello, World! from React 18')
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+//root.render(heading);
+root.render(parent);
