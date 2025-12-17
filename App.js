554cb0da0c229de6968c159e1ede76f327537fdc
Rohit Sharma <rohit.sharma@browserstack.com>
2025-12-17 17:09:24 +0530

    Chapter 3 : Laying the foundation

--- App.js
+++ App.js
@@ -16,10 +16,46 @@ const parent = React.createElement('div', {id: 'parent'},
     ]
 );
 
+const jsxHeading = <h1 id="heading">Hello, World! from JSX</h1>;
+
+//Functional Component
+const HeadingComponent = () => {
+    return <h1 id="heading">Hello, World! from Functional Component</h1>;
+};
+
+// Above can also be written as
+const HeadingComponent1 = () => <h1 id="heading">Hello, World! from Functional Component</h1>;
+
+//Or it cn also be written as
+const HeadingComponent3 = () => (<h1 id="heading" 
+      className='heading'>Hello, World! from Functional Component
+     </h1>);
 
 
 //const heading = React.createElement('h1', {id: 'heading'}, 'Hello, World! from React 18')
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 //root.render(heading);
-root.render(parent);
+
+//Way to render react element -----> root.render(parent);
+
+
+//To render React Component ------>
+//root.render(HeadingComponent());
+//or 
+root.render(<HeadingComponent />);
+
+
+//What is Component Composition? 
+//Using Component inside another Component
+
+const AppComponent = () => {
+    return (
+        <div id="app">
+            <HeadingComponent />  
+            <h2>This is a subheading from App Component</h2>
+        </div>
+    );
+}
+
+root.render(<AppComponent />);
